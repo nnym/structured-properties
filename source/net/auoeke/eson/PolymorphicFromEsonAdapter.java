9082181850b3174e53cc8e377411ce0f4c8788c7
Mohamed Maaliki <me@auoeke.net>
2022-03-26 21:05:53 +0000

    correct Javadoc

--- source/net/auoeke/eson/PolymorphicFromEsonAdapter.java
+++ source/net/auoeke/eson/PolymorphicFromEsonAdapter.java
@@ -5,7 +5,7 @@ import net.auoeke.eson.element.EsonElement;
 public interface PolymorphicFromEsonAdapter<A, B extends EsonElement> {
     /**
      Return whether this adapter can adapt a given type from eson.
-     <b>This method is pure.</b> Its result will be permanently recorded after its first invocation.
+     <b>This method is pure.</b> If this method returns {@code true}, then it will become permanently associated with the type.
 
      @param type a type
      @return whether this adapter can adapt {@code type} to eson
